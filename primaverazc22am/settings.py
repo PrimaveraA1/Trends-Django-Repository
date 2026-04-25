9235b70c91a26daf6d38ea8e9787b0fcb1fb7991
primecodes-ops <angelakrystelle06@gmail.com>
2026-04-25 20:14:13 +0800

    initial commit

--- primaverazc22am/settings.py
+++ primaverazc22am/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p@i+=&y_2d*%b2jz#az4%8jia^%mred$u=(^voqenkbdinwxg)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
